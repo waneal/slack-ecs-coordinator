828d67d9d00f605efc66ddccfea2b78a5b54e80b
waneal <37803125+waneal@users.noreply.github.com>
2021-08-14 07:56:59 +0000

    add simple reaction function

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# Install the Slack app and get xoxb- token in advance
+app = App(token=os.environ["SLACK_BOT_TOKEN"])
+
+# Add functionality here
+@app.event("app_mention")
+def handle_mention(body, say, logger):
+    logger.info(body)
+    say("hello")
+
+if __name__ == "__main__":
+    # Create an app-level token with connections:write scope
+    handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
+    handler.start()
